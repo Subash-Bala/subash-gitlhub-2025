e6337f1324c6ed02c8686a44ca89945aec6d12c6
Subash <bmani0413@gmail.com>
2025-09-22 08:57:56 +0530

    Update ESLint config to ignore docs folder

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['build', 'reports'] },
+  { ignores: ['build', 'reports', 'docs/**'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
