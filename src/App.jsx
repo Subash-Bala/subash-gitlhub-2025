fbf91e1692cea62126cd6fab2caf78247e073b97
Subash <bmani0413@gmail.com>
2025-09-22 10:05:01 +0530

    Updated App.jsx file to reflect new changes in website

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <div className="card">
         <h2>AWS DevOps Program:2025</h2>
         <p>
-          Dive into the world of CI/CD and learn the effective ways to integrate it into your pipeline with Subash
+          Dive into the world of CI/CD and learn the effective ways to integrate it into your pipeline with Kevin
         </p>
         <p>
           Hint: Edit <code>src/App.jsx</code> to make changes to this page.
